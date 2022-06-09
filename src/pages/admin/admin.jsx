48a2313de033a4184d41562520063b1b208a6d1e
Simon <milkjhang@hotmail.com>
2022-06-10 00:56:23 +0800

    temp

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, { lazy } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { Layout } from "antd";
 import memoryUtils from "../../utils/memoryUtils";
 // component
 import LetNav from "../../components/leftNav/letNav";
 import Header from "../../components/header/header";
+// pages
+// const Home = lazy(() => import("../home/home"));
+// const Role = lazy(() => import("../role/role"));
+// const User = lazy(() => import("../user/user"));
+// const Products = lazy(() => import("../products/products"));
+// const BarChart = lazy(() => import("../chart/barChart"));
+// const LineChart = lazy(() => import("../chart/lineChart"));
+// const PieChart = lazy(() => import("../chart/pieChart"));
+// const Category = lazy(() => import("../category/category"));
+import Home from "../home/home";
+import Role from "../role/role";
+import User from "../user/user";
+import Products from "../products/products";
+import BarChart from "../chart/barChart";
+import LineChart from "../chart/lineChart";
+import PieChart from "../chart/pieChart";
+import Category from "../home/home";
 
 const { Footer, Sider, Content } = Layout;
 const Admin = () => {
@@ -19,7 +36,11 @@ const Admin = () => {
       </Sider>
       <Layout>
         <Header>Header</Header>
-        <Content style={{ backgroundColor: "#fff" }}>Content</Content>
+        <Content style={{ backgroundColor: "#fff" }}>
+          <Switch>
+            <Route to="/" component={Home}></Route>
+          </Switch>
+        </Content>
         <Footer style={{ textAlign: "center" }}>For Practice</Footer>
       </Layout>
     </Layout>
