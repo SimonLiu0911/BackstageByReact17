48770db29e5676dc40846f47bdfb3eb9dc5b4bbf
Simon <milkjhang@hotmail.com>
2022-06-09 18:32:09 +0800

    temp

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div>
+			Headrer
+		</div>
+	);
+}
+
+export default Header;
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import memoryUtils from '../../utils/memoryUtils';
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import memoryUtils from "../../utils/memoryUtils";
+import { Layout } from "antd";
 
+const { Header, Footer, Sider, Content } = Layout;
 const Admin = () => {
-	const user = memoryUtils.user;
-	if (!user) {
-		return <Redirect to="/login" />
-	}
-	return (
-		<div>
-			Admin {user.email}
-		</div>
-	);
-}
+  const user = memoryUtils.user;
+  if (!user) {
+    return <Redirect to="/login" />;
+  }
+  return (
+    <Layout style={{ height: "100%" }}>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default Admin;
