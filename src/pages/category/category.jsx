621d4016c2255f3aa3b1c9900ead3d47e3f21ef2
Simon <simon.liu@cloud-interactive.com>
2022-06-21 16:49:46 +0800

    CategoryPage: temp api

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -3,53 +3,55 @@ import { Card, Button, Table } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import { reqCategories } from "../../assets/api";
 
-const columns = [
-  {
-    title: "分類名稱",
-    dataIndex: "name",
-  },
-  {
-    title: "操作",
-    width: 300,
-    render: () => (
-      <span>
-        <Button onClick={test}>修改分類</Button>
-        <Button>查看子分類</Button>
-      </span>
-    ),
-  },
-];
-
 const Category = () => {
-  const [category, setCategory] = useState([
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [subCategories, setSubCategories] = useState([]);
+  const [parentId, setParentId] = useState("0");
+  const getCategories = async () => {
+    const response = await reqCategories(parentId);
+    if (parentId === '0') {
+      setCategories(response.data)
+    } else {
+      setSubCategories(response.data)
+    }
+  };
+  const columns = [
     {
-      id: "1",
-      name: "Mike",
+      title: "分類名稱",
+      dataIndex: "name",
     },
-  ]);
-  const getCategory = async () => {
-    const response = await reqCategories("0");
-	setCategory(response.data)
-  };
-  const handleAdd = () => {
-    console.log(1);
-  };
+    {
+      title: "操作",
+      width: 300,
+      render: (categories) => (
+        <span>
+          <Button>修改分類</Button>
+          <Button onClick={() => showSubCategories(categories)}>查看子分類</Button>
+        </span>
+      ),
+    },
+  ];
+  const showSubCategories = (categories) => {
+    console.log(categories);
+  }
+
   const title = "一級分類表";
   const extra = (
-    <Button type="primary" onClick={handleAdd}>
+    <Button type="primary">
       <PlusOutlined />
       新增
     </Button>
   );
 
   useEffect(() => {
-    getCategory();
+    getCategories();
   }, []);
 
   return (
     <div>
       <Card title={title} extra={extra}>
-        <Table dataSource={category} columns={columns} bordered rowKey="id" loading={true} />
+        <Table dataSource={parentId === "0" ? categories : subCategories} columns={columns} bordered rowKey="id" loading={isLoading} />
       </Card>
     </div>
   );
