f993afeb599b73f8840b3b24ea5c09065740aad9
Simon <milkjhang@hotmail.com>
2022-06-23 01:14:40 +0800

    CategoryPage: temp subCategories

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -8,13 +8,16 @@ const Category = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [subCategories, setSubCategories] = useState([]);
   const [parentId, setParentId] = useState("0");
+  const [parentName, setParentName] = useState("");
   const getCategories = async () => {
+    setIsLoading(true);
     const response = await reqCategories(parentId);
-    if (parentId === '0') {
-      setCategories(response.data)
+    if (parentId === "0") {
+      setCategories(response.data);
     } else {
-      setSubCategories(response.data)
+      setSubCategories(response.data);
     }
+    setIsLoading(false);
   };
   const columns = [
     {
@@ -27,16 +30,31 @@ const Category = () => {
       render: (categories) => (
         <span>
           <Button>修改分類</Button>
-          <Button onClick={() => showSubCategories(categories)}>查看子分類</Button>
+          {parentId === "0" ? (
+            <Button onClick={() => showSubCategories(categories)}>
+              查看子分類
+            </Button>
+          ) : null}
         </span>
       ),
     },
   ];
+  // 顯示指定一級分類對象的二子列表
   const showSubCategories = (categories) => {
-    console.log(categories);
-  }
+    setParentName(categories.name);
+    setParentId(categories.parentId);
+    console.log(parentName, parentId);
+  };
 
-  const title = "一級分類表";
+  const title =
+    parentId === "0" ? (
+      "一級分類表"
+    ) : (
+      <span>
+        <Button>一級分類表</Button>
+        <span>{parentName}</span>
+      </span>
+    );
   const extra = (
     <Button type="primary">
       <PlusOutlined />
@@ -51,7 +69,13 @@ const Category = () => {
   return (
     <div>
       <Card title={title} extra={extra}>
-        <Table dataSource={parentId === "0" ? categories : subCategories} columns={columns} bordered rowKey="id" loading={isLoading} />
+        <Table
+          dataSource={parentId === "0" ? categories : subCategories}
+          columns={columns}
+          bordered
+          rowKey="id"
+          loading={isLoading}
+        />
       </Card>
     </div>
   );
